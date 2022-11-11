12a2455e30f2b7bafbfa1ab4737abd39445f4e07
Dev Gill <26435947+sdevgill@users.noreply.github.com>
2022-11-11 12:17:35 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Posts App with Idiorm
+
+This is a simple app that uses the [Idiorm](https://idiorm.readthedocs.io/en/latest/configuration.html)
+library to connect to a MySQL database and perform CRUD operations.
---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Idiorm - Posts App</title>
+  <link rel="stylesheet" href="static/index.css">
+</head>
+
+<body>
+
+  <!-- App Container -->
+  <div class="container">
+    <!-- Navbar -->
+    <div class="navbar">
+      <div class="logo_div">
+        <a href="index.php">
+          <h1>Idiorm - Posts App</h1>
+        </a>
+      </div>
+      <ul>
+        <li><a class="active" href="index.php">Home</a></li>
+        <li><a href="#signup">Sign Up</a></li>
+        <li><a href="#login">Login</a></li>
+      </ul>
+    </div>
+    <!-- Navbar -->
+
+    <!-- Posts -->
+    <div class="content">
+      <h2 class="content-title">Recent Posts</h2>
+      <hr>
+      <!-- More content still to come here ... -->
+    </div>
+    <!-- Posts -->
+
+    <!-- Footer -->
+    <div class="footer">
+      <p>MyViewers &copy; <?php echo date('Y'); ?></p>
+    </div>
+    <!-- Footer -->
+
+  </div>
+  <!-- App Container -->
+
+</body>
+
+</html>
